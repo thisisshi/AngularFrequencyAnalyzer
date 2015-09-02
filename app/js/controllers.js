5f0fe2ddb8578a502e9c239bc848ddfc7938e0be
Sonny <theli@Outlook.com>
2015-09-02 12:49:34 -0400

    removing images

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,6 +10,9 @@ cryptApp.controller('CryptCtrl', function ($scope){
 	$scope.hash = {
 		a: 0, b: 0, c: 0, d: 0,	e: 0, f: 0,	g: 0, h: 0, i: 0, j: 0, k: 0, l: 0, m: 0, n: 0, o: 0, p: 0,	q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0, x: 0,	y: 0, z: 0
 	};
+	$scope.percenthash = {
+		a: 0, b: 0, c: 0, d: 0,	e: 0, f: 0,	g: 0, h: 0, i: 0, j: 0, k: 0, l: 0, m: 0, n: 0, o: 0, p: 0,	q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0, x: 0,	y: 0, z: 0
+	};
 
 	$scope.calcInstances = function(){
 		for (var i = 0; i < $scope.cryptInput.length; i++) {
@@ -98,6 +101,6 @@ cryptApp.controller('CryptCtrl', function ($scope){
 
 	$scope.calcFreq = function(){
 		$scope.calcInstances();
-		
+
 	}
 })
