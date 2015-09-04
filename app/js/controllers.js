49d8aeea5487c0d95342325b76cbef94d5d29acc
Sonny <theli@Outlook.com>
2015-09-04 12:25:32 -0400

    Cleaning up calculating instances and maximum

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,100 +4,33 @@
 
 var cryptApp = angular.module('cryptApp', []);
 cryptApp.controller('CryptCtrl', function ($scope){
-	$scope.name = "Encrypted Text";
 	$scope.cryptInput = "Input Encrypted Text";
-	$scope.a = 0;
 	$scope.hash = {
-		a: 0, b: 0, c: 0, d: 0,	e: 0, f: 0,	g: 0, h: 0, i: 0, j: 0, k: 0, l: 0, m: 0, n: 0, o: 0, p: 0,	q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0, x: 0,	y: 0, z: 0
+		A: 0, B: 0, C: 0, D: 0,	E: 0, F: 0,	G: 0, H: 0, I: 0, J: 0, K: 0, L: 0, M: 0, N: 0, O: 0, P: 0,	Q: 0, R: 0, S: 0, T: 0, U: 0, V: 0, W: 0, X: 0,	Y: 0, Z: 0
 	};
-	$scope.master = {
-		a: 0, b: 0, c: 0, d: 0,	e: 0, f: 0,	g: 0, h: 0, i: 0, j: 0, k: 0, l: 0, m: 0, n: 0, o: 0, p: 0,	q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0, x: 0,	y: 0, z: 0
+	$scope.hashmaster = {
+		A: 0, B: 0, C: 0, D: 0,	E: 0, F: 0,	G: 0, H: 0, I: 0, J: 0, K: 0, L: 0, M: 0, N: 0, O: 0, P: 0,	Q: 0, R: 0, S: 0, T: 0, U: 0, V: 0, W: 0, X: 0,	Y: 0, Z: 0
 	};
-	$scope.calcFreq = function(){
-		$scope.hash = angular.copy($scope.master);
 
+	$scope.max = {character: "", value: 0};
+	$scope.maxmaster = {character: "", value: 0};
+
+
+	$scope.calcInstances = function(){
+		$scope.cryptInput = angular.uppercase($scope.cryptInput);
+		$scope.hash = angular.copy($scope.hashmaster);
+		$scope.max = angular.copy($scope.maxmaster)
+		// Gets the Instances of each character
 		for (var i = 0; i < $scope.cryptInput.length; i++) {
-			switch($scope.cryptInput[i]){
-				case "a":
-				$scope.hash.a = $scope.hash.a + 1
-				break;
-				case "b":
-				$scope.hash.b = $scope.hash.b + 1
-				break;
-				case "c":
-				$scope.hash.c = $scope.hash.c + 1
-				break;
-				case "d":	
-				$scope.hash.d = $scope.hash.d + 1
-				break;
-				case "e":
-				$scope.hash.e = $scope.hash.e + 1
-				break;
-				case "f":
-				$scope.hash.f = $scope.hash.f + 1
-				break;
-				case "g":
-				$scope.hash.g = $scope.hash.g + 1
-				break;
-				case "h":
-				$scope.hash.h = $scope.hash.h + 1
-				break;
-				case "i":
-				$scope.hash.i = $scope.hash.i + 1
-				break;
-				case "j":
-				$scope.hash.j = $scope.hash.j + 1
-				break;
-				case "k":
-				$scope.hash.k = $scope.hash.k + 1
-				break;
-				case "l":
-				$scope.hash.l = $scope.hash.l + 1
-				break;
-				case "m":
-				$scope.hash.m = $scope.hash.m + 1
-				break;
-				case "n":
-				$scope.hash.n = $scope.hash.n + 1
-				break;
-				case "o":
-				$scope.hash.o = $scope.hash.o + 1
-				break;
-				case "p":
-				$scope.hash.p = $scope.hash.p + 1
-				break;
-				case "q":
-				$scope.hash.q = $scope.hash.q + 1
-				break;
-				case "r":
-				$scope.hash.r = $scope.hash.r + 1
-				break;
-				case "s":
-				$scope.hash.s = $scope.hash.s + 1
-				break;
-				case "t":
-				$scope.hash.t = $scope.hash.t + 1
-				break;
-				case "u":
-				$scope.hash.u = $scope.hash.u + 1
-				break;
-				case "v":
-				$scope.hash.v = $scope.hash.v + 1
-				break;
-				case "w":
-				$scope.hash.w = $scope.hash.w + 1
-				break;
-				case "x":
-				$scope.hash.x = $scope.hash.x + 1
-				break;
-				case "y":
-				$scope.hash.y = $scope.hash.y + 1
-				break;
-				case "z":
-				$scope.hash.z = $scope.hash.z + 1
-				break;
+
+			$scope.hash[$scope.cryptInput[i]] =  $scope.hash[$scope.cryptInput[i]] + 1
+			if ($scope.max.value < $scope.hash[$scope.cryptInput[i]] + 1){
+				$scope.max.character = $scope.cryptInput[i];
+				$scope.max.value = $scope.hash[$scope.cryptInput[i]]
 			}
 		};
 	};
-
+	$scope.calcMax = function(){
+		return $scope.max.character;
+	};
 })
