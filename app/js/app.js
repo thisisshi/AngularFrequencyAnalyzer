7df3e806f03f17301a166c360afb2d158bf17f73
Sonny <theli@Outlook.com>
2015-09-09 11:42:33 -0400

    added caesar shift

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,8 @@
-<script>
-  new Tablesort(document.getElementById('instances'));
-</script>
+$(".dropdown-menu li a").click(function(){
+  var selText = $(this).text();
+  $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
+});
+
+$("#btnSearch").click(function(){
+	alert($('.btn-select').text()+", "+$('.btn-select2').text());
+});
